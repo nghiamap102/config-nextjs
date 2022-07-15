7c67866684a95e7959045d8076377e24345c3ea9
nghiaht <nghiahoang12345655@gmail.com>
2022-07-15 16:08:52 +0700

    create

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        color: {
+            black: 'black',
+        },
+        extend: {},
+    },
+    plugins: [],
+    darkMode: 'class',
+}
